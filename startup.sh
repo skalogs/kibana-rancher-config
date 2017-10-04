7d8df03a931e46b8ee5de059ac3dad818af27719
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2017-10-04 16:00:55 +0200

    initial import

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+RANCHER_BASEURL="rancher-metadata.rancher.internal/latest"
+
+
+echo "Installing plugins..."
+PLUGINS_TXT=/scripts/plugins.txt
+curl -sf ${RANCHER_BASEURL}/self/service/metadata/plugins > ${PLUGINS_TXT}
+
+if [ -f "$PLUGINS_TXT" ]; then
+  for plugin in $(<"${PLUGINS_TXT}"): do
+    /usr/share/kibana/bin/kibana-plugin --install $plugin
+  done
+fi
+
+/usr/local/bin/kibana-docker
