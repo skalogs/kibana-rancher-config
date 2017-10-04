6393b5467ae5b8187d58d0942c49ffcd35ed882b
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2017-10-04 17:11:15 +0200

    minor fixes /tmp is writable with kibana account default folder isn't add some debugging message use proper plugin install syntax

--- startup.sh
+++ startup.sh
@@ -5,12 +5,13 @@ RANCHER_BASEURL="rancher-metadata.rancher.internal/latest"
 
 
 echo "Installing plugins..."
-PLUGINS_TXT=/scripts/plugins.txt
+PLUGINS_TXT="/tmp/plugins.txt"
 curl -sf ${RANCHER_BASEURL}/self/service/metadata/plugins > ${PLUGINS_TXT}
 
 if [ -f "$PLUGINS_TXT" ]; then
-  for plugin in $(<"${PLUGINS_TXT}"): do
-    /usr/share/kibana/bin/kibana-plugin --install $plugin
+  for plugin in $(<"${PLUGINS_TXT}"); do
+    echo "Installing $plugin"
+    /usr/share/kibana/bin/kibana-plugin install $plugin
   done
 fi
 
