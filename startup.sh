90a99ed10b56ec7026fe49abbc7ba7258bbbc94d
Nicolas Poirier <nicolas.npoirier@gmail.com>
2017-10-09 15:57:43 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,13 +8,6 @@ echo "Installing plugins..."
 PLUGINS_TXT="/tmp/plugins.txt"
 curl -sf ${RANCHER_BASEURL}/self/service/metadata/plugins > ${PLUGINS_TXT}
 
-if [ -f "$PLUGINS_TXT" ]; then
-  for plugin in $(<"${PLUGINS_TXT}"); do
-    echo "Installing $(basename ${plugin})"
-    /usr/share/kibana/bin/kibana-plugin install $plugin || true
-  done
-fi
-
 if [ -n "$CUSTOM_KIBANA_LOOK_AND_FEEL" ]; then
   CUSTOM_KIBANA="/tmp/kibana.tar.gz"
   curl -Lsf ${$CUSTOM_KIBANA_LOOK_AND_FEEL} > ${CUSTOM_KIBANA}
@@ -24,5 +17,14 @@ if [ -n "$CUSTOM_KIBANA_LOOK_AND_FEEL" ]; then
   rm -rf /usr/share/kibana/optimize/bundles
   echo "clean bundles finish ..."
 fi
+
+if [ -f "$PLUGINS_TXT" ]; then
+  for plugin in $(<"${PLUGINS_TXT}"); do
+    echo "Installing $(basename ${plugin})"
+    /usr/share/kibana/bin/kibana-plugin install $plugin || true
+  done
+fi
+
+
 
 /usr/local/bin/kibana-docker
