232127063b835000a6f359934e10ae38235d45b9
christophefromparis <christophe.cosnefroy@gmail.com>
2018-03-02 11:12:19 +0100

    Adds Kibana.yml

--- startup.sh
+++ startup.sh
@@ -3,6 +3,14 @@ set -e
 
 RANCHER_BASEURL="rancher-metadata.rancher.internal/latest"
 
+response=$(curl --write-out %{http_code} --silent --output /dev/null http://${RANCHER_BASEURL}/self/service/metadata/kibana-config)
+if [ "$response" -eq 200 ]
+then
+  echo "Installing custom Kibana config"
+  mkdir -p /usr/share/kibana/config
+  # kibana.yml
+  curl -sf ${RANCHER_BASEURL}/self/service/metadata/kibana-config > /usr/share/kibana/config/kibana.yml
+fi
 
 echo "Installing plugins..."
 PLUGINS_TXT="/tmp/plugins.txt"
